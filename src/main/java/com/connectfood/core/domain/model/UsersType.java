53502520e65f5d66b5c96d8cc006dc38572bd316
Lucas Mumbarra <lucassantosm.2021@gmail.com>
2025-12-01 00:44:09 -0300

    chore: ajusta regra de negócio

--- src/main/java/com/connectfood/core/domain/model/UsersType.java
+++ src/main/java/com/connectfood/core/domain/model/UsersType.java
@@ -19,19 +19,16 @@ public class UsersType {
       throw new BadRequestException("Name is required");
     }
 
-    this.uuid = uuid;
+    if (name.length() < 3 || name.length() > 255) {
+      throw new BadRequestException("Name must be between 3 and 255 characters");
+    }
+
+    this.uuid = uuid == null ? UUID.randomUUID() : uuid;
     this.name = name;
     this.description = description;
   }
 
   public UsersType(final String name, final String description) {
-
-    if (name == null || name.isBlank()) {
-      throw new BadRequestException("Name is required");
-    }
-
-    this.uuid = UUID.randomUUID();
-    this.name = name;
-    this.description = description;
+    this(null, name, description);
   }
 }
