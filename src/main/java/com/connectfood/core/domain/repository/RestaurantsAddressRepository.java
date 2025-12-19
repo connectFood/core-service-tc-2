3b80a5ac23b264b78eb69c29cb10d9120af3ef0d
caiotelesz <caioteles1267@gmail.com>
2025-12-19 14:42:38 -0300

    feat: add missing CRUD methods to RestaurantsAddress repository

--- src/main/java/com/connectfood/core/domain/repository/RestaurantsAddressRepository.java
+++ src/main/java/com/connectfood/core/domain/repository/RestaurantsAddressRepository.java
@@ -1,7 +1,9 @@
 package com.connectfood.core.domain.repository;
 
 import com.connectfood.core.domain.model.RestaurantsAddress;
+import com.connectfood.core.domain.model.commons.PageModel;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -9,7 +11,12 @@ public interface RestaurantsAddressRepository {
 
   RestaurantsAddress save(RestaurantsAddress restaurantsAddress);
 
+  RestaurantsAddress update(UUID uuid, RestaurantsAddress restaurantsAddress, UUID addressTypeUuid);
+
   Optional<RestaurantsAddress> findByRestaurantsUuid(UUID uuid);
+
+  PageModel<List<RestaurantsAddress>> findAll(UUID restaurantsUuid, UUID addressUuid, Integer page, Integer size,
+      String sort, String direction);
 
   void delete(UUID uuid);
 }
