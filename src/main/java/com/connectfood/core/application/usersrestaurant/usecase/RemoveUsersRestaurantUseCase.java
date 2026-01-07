86c8b3f83f02c67d61f24900d12359b62551c7f9
Suelen Peres <azm2.SuelenSP@sefazrsgov.onmicrosoft.com>
2026-01-07 10:33:38 -0300

    feat(users-restaurant): implementa casos de uso de busca, atualização e remoção e padroniza repositório

--- src/main/java/com/connectfood/core/application/usersrestaurant/usecase/RemoveUsersRestaurantUseCase.java
+++ src/main/java/com/connectfood/core/application/usersrestaurant/usecase/RemoveUsersRestaurantUseCase.java
@@ -1,5 +1,29 @@
 package com.connectfood.core.application.usersrestaurant.usecase;
 
+import java.util.UUID;
+
+import com.connectfood.core.domain.exception.NotFoundException;
+import com.connectfood.core.domain.repository.UsersRestaurantRepository;
+
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
 public class RemoveUsersRestaurantUseCase {
-    
+
+  private final UsersRestaurantRepository repository;
+
+  public RemoveUsersRestaurantUseCase(final UsersRestaurantRepository repository) {
+    this.repository = repository;
+  }
+
+  @Transactional
+  public void execute(final UUID usersUuid) {
+
+    final var usersRestaurant =
+        repository.findByUsersUuid(usersUuid)
+            .orElseThrow(() -> new NotFoundException("Users restaurant not found"));
+
+    repository.delete(usersRestaurant.getUuid());
+  }
 }
--- src/main/java/com/connectfood/core/domain/repository/UsersRestaurantRepository.java
+++ src/main/java/com/connectfood/core/domain/repository/UsersRestaurantRepository.java
@@ -1,15 +1,29 @@
 package com.connectfood.core.domain.repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 import com.connectfood.core.domain.model.UsersRestaurant;
+import com.connectfood.core.domain.model.commons.PageModel;
 
 public interface UsersRestaurantRepository {
 
   UsersRestaurant save(UsersRestaurant usersRestaurant);
 
+  UsersRestaurant update(UUID uuid, UsersRestaurant usersRestaurant);
+
+  Optional<UsersRestaurant> findById(UUID uuid);
+
   Optional<UsersRestaurant> findByUsersUuid(UUID usersUuid);
+
+  PageModel<List<UsersRestaurant>> findAllByRestaurantUuid(
+      UUID restaurantUuid,
+      Integer page,
+      Integer size,
+      String sort,
+      String direction
+  );
 
   void delete(UUID uuid);
 }
